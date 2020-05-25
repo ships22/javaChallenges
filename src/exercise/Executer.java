878429031633b9257649074239411dfb4638850b
ships22 <sshipsss@gmail.com>
2020-05-25 16:36:19 +0200

    word counter finished

--- src/exercise/Executer.java
+++ src/exercise/Executer.java
@@ -1,9 +1,18 @@
 package exercise;
 
+import java.util.Map;
+
+import exercise.tasks.WordCounter;
+
 public class Executer {
 
 	public static void main(String[] args) {
-		// Word counter  from array of string-
+		// Word counter  from array of string -
+		WordCounter wordCounter = new WordCounter();
+		String[] strings = {"a", "a", "b", "b", "c", "c", "a"};
+		Map<String, Integer> map = wordCounter.wordCounter(strings);
+		System.out.println("La résultat de word count est :  " + map);
+		
 
 	}
 
